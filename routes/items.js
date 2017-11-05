324c566e91b5418f29df828c21f894ef8d7cba9e
Francisco Montufar <franciscomontufar28@gmail.com>
2017-11-05 11:40:05 -0500

    preferences backend using as json output

--- routes/items.js
+++ routes/items.js
@@ -11,14 +11,15 @@ function middleware(req, res, next) {
     next();
 };
 
-router.get("/", (req, res, next) => {
+router.get("/preferences", (req, res, next) => {
 
      let userNameId = req.query.usernameid;
      let aisleNameOne = req.query.aislenameone;
      let aisleNameTwo = req.query.aislenametwo;
     //res.send({ idusuario: idusuario, aisleone: aisleone, aisletwo: aisletwo });
 
-    req.db.query("SELECT userpreferences.itemcode FROM userpreferences inner join itemslocation on itemslocation.itemcode = userpreferences.itemcode where (userpreferences.usercodenum = "+"'"+userNameId+"'"+" and (itemslocation.aislelocation = "+"'"+aisleNameOne+"'"+" or itemslocation.aislelocation = "+"'"+aisleNameTwo+"'"+")) order by rand() Limit 1",(err,result)=>{
+    //req.db.query("SELECT userpreferences.itemcode FROM userpreferences inner join itemslocation on itemslocation.itemcode = userpreferences.itemcode where (userpreferences.usercodenum = "+"'"+userNameId+"'"+" and (itemslocation.aislelocation = "+"'"+aisleNameOne+"'"+" or itemslocation.aislelocation = "+"'"+aisleNameTwo+"'"+")) order by rand() Limit 1",(err,result)=>{
+    req.db.query("SELECT userpreferences.itemcode FROM userpreferences inner join itemsfeatureslocation on itemsfeatureslocation.itemcode = userpreferences.itemcode where (userpreferences.usercodenum = "+"'"+userNameId+"'"+" and (itemsfeatureslocation.aislelocation = "+"'"+aisleNameOne+"'"+" or itemsfeatureslocation.aislelocation = "+"'"+aisleNameTwo+"'"+")) order by rand() Limit 1",(err,result)=>{
         //console.log("SELECT userpreferences.itemcode FROM userpreferences inner join itemslocation on itemslocation.itemcode = userpreferences.itemcode where (userpreferences.usercodenum = "+"'"+userNameId+"'"+" and (itemslocation.aislelocation = "+"'"+aisleNameOne+"'"+" or itemslocation.aislelocation = "+"'"+aisleNameTwo+"'"+")) order by rand() Limit 1");
         if(err){
             res.status(500).send("Error al consultar items");
@@ -27,8 +28,9 @@ router.get("/", (req, res, next) => {
 
         }else{
             var itemRandom = result[0].itemcode;
-            //console.log("SELECT similarity.id_second_item, itemslocation.nameitem FROM similarity inner join itemslocation on itemslocation.itemcode = similarity.id_second_item where similarity.id_first_item = "+itemRandom+" and similarity.coef_similarity > 40 and (itemslocation.aislelocation = "+"'"+aisleNameOne+"'"+" or itemslocation.aislelocation = "+"'"+aisleNameOne+"'"+")order by rand() Limit 5");
-            req.db.query("SELECT similarity.id_second_item, itemslocation.nameitem FROM similarity inner join itemslocation on itemslocation.itemcode = similarity.id_second_item where similarity.id_first_item = "+itemRandom+" and similarity.coef_similarity > 40 and (itemslocation.aislelocation = "+"'"+aisleNameOne+"'"+" or itemslocation.aislelocation = "+"'"+aisleNameOne+"'"+")order by rand() Limit 5",(error,resultrandom)=>{
+            console.log("SELECT itemsfeatureslocation.nameitem as itemname,itemsfeatureslocation.description as itemdescription, itemsfeatureslocation.urlimage as itemurl, itemsfeatureslocation.sodium as itemsodium, itemsfeatureslocation.sugar as itemsugar FROM similarity inner join itemsfeatureslocation on itemsfeatureslocation.itemcode = similarity.id_second_item where similarity.id_first_item = "+itemRandom+" and similarity.coef_similarity BETWEEN 40 AND 80 and (itemsfeatureslocation.aislelocation = "+"'"+aisleNameOne+"'"+" or itemsfeatureslocation.aislelocation = "+"'"+aisleNameOne+"'"+")order by rand() Limit 5");
+            //req.db.query("SELECT similarity.id_second_item, itemslocation.nameitem FROM similarity inner join itemslocation on itemslocation.itemcode = similarity.id_second_item where similarity.id_first_item = "+itemRandom+" and similarity.coef_similarity > 40 and (itemslocation.aislelocation = "+"'"+aisleNameOne+"'"+" or itemslocation.aislelocation = "+"'"+aisleNameOne+"'"+")order by rand() Limit 5",(error,resultrandom)=>{
+            req.db.query("SELECT itemsfeatureslocation.nameitem as itemname,itemsfeatureslocation.description as itemdescription, itemsfeatureslocation.urlimage as itemurl, itemsfeatureslocation.sodium as itemsodium, itemsfeatureslocation.sugar as itemsugar FROM similarity inner join itemsfeatureslocation on itemsfeatureslocation.itemcode = similarity.id_second_item where similarity.id_first_item = "+itemRandom+" and similarity.coef_similarity BETWEEN 40 AND 80 and (itemsfeatureslocation.aislelocation = "+"'"+aisleNameOne+"'"+" or itemsfeatureslocation.aislelocation = "+"'"+aisleNameOne+"'"+")order by rand() Limit 5",(error,resultrandom)=>{
                 res.send(resultrandom);
             });
             
@@ -38,6 +40,36 @@ router.get("/", (req, res, next) => {
     
 
 });
+
+router.get("/morepreferred", (req, res, next) => {
+    
+         let userNameId = req.query.usernameid;
+         let aisleNameOne = req.query.aislenameone;
+         let aisleNameTwo = req.query.aislenametwo;
+        //res.send({ idusuario: idusuario, aisleone: aisleone, aisletwo: aisletwo });
+    
+        //req.db.query("SELECT userpreferences.itemcode FROM userpreferences inner join itemslocation on itemslocation.itemcode = userpreferences.itemcode where (userpreferences.usercodenum = "+"'"+userNameId+"'"+" and (itemslocation.aislelocation = "+"'"+aisleNameOne+"'"+" or itemslocation.aislelocation = "+"'"+aisleNameTwo+"'"+")) order by rand() Limit 1",(err,result)=>{
+        req.db.query("SELECT userpreferences.itemcode FROM userpreferences inner join itemsfeatureslocation on itemsfeatureslocation.itemcode = userpreferences.itemcode where (userpreferences.usercodenum = "+"'"+userNameId+"'"+" and (itemsfeatureslocation.aislelocation = "+"'"+aisleNameOne+"'"+" or itemsfeatureslocation.aislelocation = "+"'"+aisleNameTwo+"'"+")) order by rand() Limit 1",(err,result)=>{
+            //console.log("SELECT userpreferences.itemcode FROM userpreferences inner join itemslocation on itemslocation.itemcode = userpreferences.itemcode where (userpreferences.usercodenum = "+"'"+userNameId+"'"+" and (itemslocation.aislelocation = "+"'"+aisleNameOne+"'"+" or itemslocation.aislelocation = "+"'"+aisleNameTwo+"'"+")) order by rand() Limit 1");
+            if(err){
+                res.status(500).send("Error al consultar items");
+            }else if(result.length==0){
+                res.status(404).send({Err:"No se obtuvieron resultados"});
+    
+            }else{
+                var itemRandom = result[0].itemcode;
+                console.log("SELECT itemsfeatureslocation.nameitem as itemname,itemsfeatureslocation.description as itemdescription, itemsfeatureslocation.urlimage as itemurl, itemsfeatureslocation.sodium as itemsodium, itemsfeatureslocation.sugar as itemsugar FROM similarity inner join itemsfeatureslocation on itemsfeatureslocation.itemcode = similarity.id_second_item where similarity.coef_similarity = 100 and (itemsfeatureslocation.aislelocation = "+"'"+aisleNameOne+"'"+" or itemsfeatureslocation.aislelocation = "+"'"+aisleNameOne+"'"+")order by rand() Limit 1");
+                //req.db.query("SELECT similarity.id_second_item, itemslocation.nameitem FROM similarity inner join itemslocation on itemslocation.itemcode = similarity.id_second_item where similarity.id_first_item = "+itemRandom+" and similarity.coef_similarity > 40 and (itemslocation.aislelocation = "+"'"+aisleNameOne+"'"+" or itemslocation.aislelocation = "+"'"+aisleNameOne+"'"+")order by rand() Limit 5",(error,resultrandom)=>{
+                req.db.query("SELECT itemsfeatureslocation.nameitem as itemname,itemsfeatureslocation.description as itemdescription, itemsfeatureslocation.urlimage as itemurl, itemsfeatureslocation.sodium as itemsodium, itemsfeatureslocation.sugar as itemsugar FROM similarity inner join itemsfeatureslocation on itemsfeatureslocation.itemcode = similarity.id_second_item where similarity.coef_similarity = 100 and (itemsfeatureslocation.aislelocation = "+"'"+aisleNameOne+"'"+" or itemsfeatureslocation.aislelocation = "+"'"+aisleNameOne+"'"+")order by rand() Limit 1",(error,resultrandom)=>{
+                    res.send(resultrandom);
+                });
+                
+            }
+        });
+    
+        
+    
+    });
 
 
 router.get("/:id", middleware, (req, res, next) => {
